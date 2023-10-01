8d1006ac56e3b5634305f060b3e3a185097b862c
Faduma <aladirie@gmail.com>
2023-10-01 18:20:33 -0400

    Constants Module cleanup

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost", // Replace with the server's IP address
-    port: 50541,       // Replace with the server's PORT number
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
@@ -13,17 +14,14 @@ const connect = function() {
   // Event handler for incoming data from the server
   conn.on("data", (data) => {
     console.log("Message from server:", data);
-
-  
   });
+
   // Register a "connect" event handler
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-
-    
     conn.write("Name: SNK");
-
   });
+
   return conn;
 };
 
